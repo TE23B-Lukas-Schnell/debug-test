019c8201d3c4651b863b2ca66af06a06b8933e07
TE23B-Lukas-Schnell <lukas.schnell@elev.ga.ntig.se>
2025-10-17 13:24:42 +0200

    𰻝༒༒ 𝼀𰻝ꬤ ꬤɷ𑴨 𐃵ꃔ꒧꒧ɷ𑴨𰻝ꕑ 𐛅ꘐ𑴨 + 𐃵𖠃𑴨𑴨ɷꬤ : ༒𖠃ꕑ𓀧𖠺ꕑ𰻝ꕑ : 𐃵𖠃𑴨𑴨 𐃵𖠃𑴨𑴨 𐃵𖠃𑴨𑴨 𐜴ꃔꕑ𑴨𐂂ɷ𑴨ꘐ ༒ꘐ𝼀 𝼀𰻝ꬤ ༒ꘐ𑴨ꘐꕑ 𰻝𐂂𑴨ꃔ𐂂

--- VariableReference.cs
+++ VariableReference.cs
@@ -1,5 +1,11 @@
 class VariableReference<variable>
 {
     public variable Value;
-    public VariableReference(variable value) => Value = value;
+    public VariableReference(variable value)
+    {
+        Value = value;
+        Console.WriteLine(Value);
+        Console.WriteLine(Value);
+    }
+
 }
